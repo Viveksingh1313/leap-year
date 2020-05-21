65ea1b685a4dfdd4e2f9b09769c19d89f3c11e7f
= <=>
2020-05-21 11:49:14 +0530

    Updating Test cases to check for leap year.

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -2,19 +2,27 @@ package com.mycompany.app;
 
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
+
+    private App app;
+
+    @Before
+    public void setup() {
+        app = new App();
+    }
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testCheckForLeapYear() {
+        Assert.assertTrue("Should be a leap year", app.checkForLeapYear(2000));
+        Assert.assertFalse("Should not be a leap year", app.checkForLeapYear(1700));
+        Assert.assertTrue("Should be a leap year", app.checkForLeapYear(2008));
+        Assert.assertFalse("Should not be a leap year", app.checkForLeapYear(2017));
     }
 }
