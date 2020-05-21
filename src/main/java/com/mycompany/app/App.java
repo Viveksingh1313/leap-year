c18728833ce573070be5d79f220488107a3c6de4
= <=>
2020-05-21 11:57:19 +0530

    Updated method a/c to Acceptance Criteria so that all test cases pass.

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -4,12 +4,20 @@ package com.mycompany.app;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
+
     public boolean checkForLeapYear(int year) {
-    return true;
+        if (year % 4 != 0) {
+            return false;
+        } else if (year % 400 == 0) {
+            return true;
+        } else if (year % 100 == 0){
+            return false;
+        } else {
+            return true;
+        }
     }
 }
